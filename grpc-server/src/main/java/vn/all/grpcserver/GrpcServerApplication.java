39893cb49fadbbe643c37b3ba46106b62730786d
Alexander Loong <dj.thaomeo@gmail.com>
2024-08-31 22:16:38 +0700

    Run success grpc server.

--- grpc-server/src/main/java/vn/all/grpcserver/GrpcServerApplication.java
+++ grpc-server/src/main/java/vn/all/grpcserver/GrpcServerApplication.java
@@ -1,37 +1,15 @@
 package vn.all.grpcserver;
 
-import io.grpc.Server;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 @Slf4j
 public class GrpcServerApplication {
 
-    private final Server server;
-
-    public GrpcServerApplication(Server server) {
-        this.server = server;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(GrpcServerApplication.class, args);
-    }
-
-    @PostConstruct
-    public void start() throws IOException {
-        server.start();
-        log.info("gRPC server started, listening on {}", server.getPort());
-    }
-
-    @PreDestroy
-    public void stop() throws InterruptedException {
-        server.shutdown().awaitTermination();
     }
 
 }
